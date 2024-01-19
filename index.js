a17a8f7a58d48b936970bcfbe6d99eb98cb39688
Christopher <rndchris@gmail.com>
2024-01-19 13:59:45 -0600

    remove extra comments from rate limiting

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Use an external store for consistency across multiple server instances.
-})
+	limit: 500,
+	standardHeaders: 'draft-7',
+	legacyHeaders: false,
+ })
 
 const app = express();
 const port = 3500;
